d6a65ae16d1cae334427f88f4cf9069233edbde1
Leo <75311991+wendy062644@users.noreply.github.com>
2025-04-08 00:14:44 +0800

    Add files via upload

---
+++ Assignment1/test.java
@@ -0,0 +1,14 @@
+// print hello world
+{
+ print("hello world");
+ /* this is a comment // line with some /* and
+// delimiters */
+ int a = 5 + 5.5;
+}
+// this is a comment // line */ /* with /* delimiters */ before the end
+123
+1+1111
+"Hello"
+1 + 1
+1++
+s = "aa\""bb" 
---
+++ Assignment1/test1.java
@@ -0,0 +1,5 @@
+public class Test1 {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
